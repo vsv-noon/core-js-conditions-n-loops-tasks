5c1fd5f64fd936798f203c67ea9ed2093e558643
ViacheslavAnisimov <forvsn@gmail.com>
2024-02-02 05:41:42 +0700

    feat: solved new tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -159,8 +159,13 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -178,8 +183,14 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numString = `${num}`;
+  for (let i = 0; i < numString.length; i += 1) {
+    if (numString[i] === `${digit}`) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
