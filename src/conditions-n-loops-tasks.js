46f5146969a513f184682134f1704cab40da3021
vsv-noon <forvsn@gmail.com>
2025-10-19 13:07:12 +0700

    feat: solved new task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -378,8 +378,29 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+
+function shuffleChar(str, iterations) {
+  let resultStr = str;
+
+  for (let i = 1; i <= iterations; i += 1) {
+    let evenIdx = '';
+    let oddIdx = '';
+
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2 === 0) {
+        evenIdx += resultStr[j];
+      } else {
+        oddIdx += resultStr[j];
+      }
+    }
+
+    resultStr = evenIdx + oddIdx;
+    if (resultStr === str) {
+      return shuffleChar(str, iterations % i);
+    }
+  }
+
+  return resultStr;
 }
 
 /**
